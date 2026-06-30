1644b41f954153f7e91153d9a4b5da4d87edc181
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-06-30 15:03:15 +0200

    overeager commit, reinstate tqdm helper

--- timflow/version.py
+++ timflow/version.py
@@ -1,3 +1,4 @@
+import warnings
 from importlib import import_module, metadata
 from platform import python_version
 
@@ -20,7 +21,7 @@ def show_versions(optional=True) -> None:
         f"Scipy version      : {metadata.version('scipy')}\n"
         f"Pandas version     : {metadata.version('pandas')}\n"
         f"Matplotlib version : {metadata.version('matplotlib')}"
-        f"tqdm version       : {metadata.version('tqdm')}"
+        # f"tqdm version       : {metadata.version('tqdm')}"
     )
     if optional:
         msg += "\nLmFit version      : "
@@ -31,3 +32,38 @@ def show_versions(optional=True) -> None:
             msg += "Not Installed"
 
     print(msg)
+
+
+def check_tqdm_parallel(parallel):
+    """Check if tqdm is installed when parallel processing is requested.
+
+    Parameters
+    ----------
+    parallel : bool
+        Whether parallel processing is requested.
+
+    Returns
+    -------
+    parallel : bool
+        Whether parallel processing was requested and can be used.
+    process_map : function or None
+        The process_map function from tqdm if parallel processing is available, else None.
+    tqdm : class or None
+        The tqdm class from tqdm if parallel processing is available, else None.
+    """
+    if not parallel:  # short circuit when no parallel requested
+        return parallel, None, None
+    try:
+        from tqdm import tqdm
+        from tqdm.contrib.concurrent import process_map
+    except ImportError:
+        warnings.warn(
+            "Parallel requires 'tqdm'. Install 'timflow[parallel]' or 'tqdm' to"
+            " enable parallel execution. Falling back to serial execution.",
+            category=ImportWarning,
+            stacklevel=2,
+        )
+        parallel = False
+        process_map = None
+        tqdm = None
+    return parallel, process_map, tqdm
