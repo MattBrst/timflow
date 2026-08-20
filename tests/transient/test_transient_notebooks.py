0253062606f17e9313d7b0c5c1d469e1aa54d490
martinvonk <vonk.mart@gmail.com>
2026-08-20 12:11:18 +0200

    speed up some transient notebooks

--- tests/transient/test_transient_notebooks.py
+++ tests/transient/test_transient_notebooks.py
@@ -3,27 +3,29 @@ from pathlib import Path
 import pytest
 import papermill as pm
 
-from time import time
 
-NB_TRANSIENT_DIR = Path.cwd().parent.parent / "docs/transient"
+NB_DIR = Path.cwd().parent.parent / "docs/transient"
 
 def get_notebooks() -> list[Path]:
     skip = ["besselaesnew_timing.ipynb"]
-    NB_TRANSIENT_DIRS = [
-        NB_TRANSIENT_DIR / "00userguide",
-        NB_TRANSIENT_DIR / "02examples",
-        NB_TRANSIENT_DIR / "03xsections",
-        NB_TRANSIENT_DIR / "05benchmarks",
+    NB_DIRS = [
+        NB_DIR / "00userguide",
+        NB_DIR / "02examples",
+        NB_DIR / "03xsections",
+        NB_DIR / "05benchmarks",
     ]
 
     nblist = []
-    for nbdir in NB_TRANSIENT_DIRS:
+    for nbdir in NB_DIRS:
         nblist += [nb for nb in nbdir.glob("*.ipynb") if nb.name not in skip]
     return sorted(nblist)
 
 PARAMETERS = {
     "meandering_river.ipynb": {"NGR": 2},
     "horizontal_well.ipynb": {"N": 2},
+    "line_sink_well_sol.ipynb": {"NGR": 2},
+    "river_in_cross_section.ipynb": {"MAX_NFEV": 2},
+    "well_near_leaky_river.ipynb": {"N": 2},
 }
 
 # @pytest.mark.notebooks
@@ -41,9 +43,18 @@ def test_notebook(pth):
     )
     output_path.unlink()  # Remove the output notebook after execution
 
+# local run
 if __name__ == "__main__":
+    from time import time
+    times = {}
     for file in get_notebooks():
         start = time()
         test_notebook(file)
         end = time()
+        times[file] = end - start
         print(f"Execution time for {file}: {end - start:.2f} seconds")
+
+    # Print summary
+    print("\nSummary:")
+    for file, duration in times.items():
+        print(f"{file}: {duration:.2f} seconds")
