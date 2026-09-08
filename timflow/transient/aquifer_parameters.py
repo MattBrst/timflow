294c31a78dd3205bb3e580489d844fbd0915f26d
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-09-08 15:42:53 +0200

    correct check for confined models

--- timflow/transient/aquifer_parameters.py
+++ timflow/transient/aquifer_parameters.py
@@ -53,7 +53,7 @@ def param_maq(
         if len(leffaq) == 1:
             leffaq = leffaq * np.ones(naq)
         if len(leffll) == 1:
-            leffll = leffll * np.ones(naq)
+            leffll = leffll * np.ones(naq - 1)
         assert len(kaq) == naq, "Error: Length of kaq needs to be " + str(naq)
         assert len(Saq) == naq, "Error: Length of Saq needs to be " + str(naq)
         assert len(poraq) == naq, "Error: Length of poraq needs to be " + str(naq)
@@ -61,7 +61,7 @@ def param_maq(
         assert len(Sll) == naq - 1, "Error: Length of Sll needs to be " + str(naq - 1)
         assert len(porll) == naq - 1, "Error: Length of porll needs to be " + str(naq - 1)
         assert len(leffaq) == naq, "Error: Length of leffaq needs to be " + str(naq)
-        assert len(leffll) == naq, "Error: Length of leffll needs to be " + str(naq)
+        assert len(leffll) == naq - 1, "Error: Length of leffll needs to be " + str(naq - 1)
         Haq = H[::2]
         assert np.all(Haq > 0), "Error: Some thicknesses of aquifer layers are negative"
         Hll = H[1::2]
