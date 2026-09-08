475e8ed17ff873fde4f856996f8beff0f2f03261
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-09-08 15:49:46 +0200

    remove newline

--- timflow/transient/aquifer_parameters.py
+++ timflow/transient/aquifer_parameters.py
@@ -60,7 +60,6 @@ def param_maq(
         assert len(c) == naq - 1, "Error: Length of c needs to be " + str(naq - 1)
         assert len(Sll) == naq - 1, "Error: Length of Sll needs to be " + str(naq - 1)
         assert len(porll) == naq - 1, "Error: Length of porll needs to be " + str(naq - 1)
-
         Haq = H[::2]
         assert np.all(Haq > 0), "Error: Some thicknesses of aquifer layers are negative"
         Hll = H[1::2]
