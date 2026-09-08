c618902c522e3fdff1883c4102adbd4c9db00c73
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2026-09-08 15:49:05 +0200

    leffaq and leffll in confined case don't do anything, set to 0.0 always, but use correct layer dimensions

--- timflow/transient/aquifer_parameters.py
+++ timflow/transient/aquifer_parameters.py
@@ -51,17 +51,16 @@ def param_maq(
         if len(porll) == 1:
             porll = porll * np.ones(naq - 1)
         if len(leffaq) == 1:
-            leffaq = leffaq * np.ones(naq)
+            leffaq = np.zeros(naq)
         if len(leffll) == 1:
-            leffll = leffll * np.ones(naq - 1)
+            leffll = np.zeros(naq - 1)
         assert len(kaq) == naq, "Error: Length of kaq needs to be " + str(naq)
         assert len(Saq) == naq, "Error: Length of Saq needs to be " + str(naq)
         assert len(poraq) == naq, "Error: Length of poraq needs to be " + str(naq)
         assert len(c) == naq - 1, "Error: Length of c needs to be " + str(naq - 1)
         assert len(Sll) == naq - 1, "Error: Length of Sll needs to be " + str(naq - 1)
         assert len(porll) == naq - 1, "Error: Length of porll needs to be " + str(naq - 1)
-        assert len(leffaq) == naq, "Error: Length of leffaq needs to be " + str(naq)
-        assert len(leffll) == naq - 1, "Error: Length of leffll needs to be " + str(naq - 1)
+
         Haq = H[::2]
         assert np.all(Haq > 0), "Error: Some thicknesses of aquifer layers are negative"
         Hll = H[1::2]
