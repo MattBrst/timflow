2d83bd3d188f10bd17d60e40f35b19286ade2f4f
Mattijs Borst <41747007+MattBrst@users.noreply.github.com>
2026-09-22 12:51:35 +0200

    Revert "Expose neglect_steady in transient Model.headalongline"

--- timflow/transient/model.py
+++ timflow/transient/model.py
@@ -671,7 +671,7 @@ class Model:
     def strength(self, elabel, t):
         return self.elementdict[elabel].strength(t - self.tstart)
 
-    def headalongline(self, x, y, t, layers=None, neglect_steady=False):
+    def headalongline(self, x, y, t, layers=None):
         """Head along line or curve.
 
         Parameters
@@ -684,9 +684,6 @@ class Model:
             times for which grid is returned
         layers : integer, list or array, optional
             layers for which grid is returned
-        neglect_steady : bool, optional
-            if `True`, the steady-state contribution from `self.steady` (if set)
-            is not added to the result, by default `False`
 
         Returns
         -------
@@ -704,7 +701,7 @@ class Model:
         t = np.atleast_1d(t)
         h = np.zeros((Nlayers, len(t), nx))
         for i in range(nx):
-            h[:, :, i] = self.head(xg[i], yg[i], t, layers, neglect_steady=neglect_steady)
+            h[:, :, i] = self.head(xg[i], yg[i], t, layers)
         return h
 
     def disvecalongline(self, x, y, t, layers=None):
